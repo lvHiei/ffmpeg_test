519dfe8ff4abde27d8c4b08870d950b35b26bc3c
majun <majun@mail.51vv.com>
2017-06-23 13:53:44 +0800

    init ffmpeg_test

---
+++ src/ffmpeg_test.cpp
@@ -0,0 +1,63 @@
+//============================================================================
+// Name        : ffmpeg_test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+#include "clipVideo/LocalVideo.h"
+#include "avfilter/VVFilterUtil.h"
+
+void testFilter()
+{
+	const char* path = "/home/mj/disk/videotest/2017/06/22/";
+	const char* videoFile = "clip.flv";
+	const char* picFile = "zimu1.png";
+	const char* outFile = "filter.flv";
+//	const char* outFile = "filter.yuv";
+
+	char absVideo[1024] = {0};
+	char absPic[1024] = {0};
+	char absOut[1024] = {0};
+
+	strcat(absVideo, path);
+	strcat(absVideo, videoFile);
+
+	strcat(absPic, path);
+	strcat(absPic, picFile);
+
+	strcat(absOut, path);
+	strcat(absOut, outFile);
+
+	VVFilterUtil* pFilter = new VVFilterUtil();
+	pFilter->initFilter(absVideo, absPic, absOut);
+	pFilter->createFilter();
+
+	pthread_join(pFilter->getThreadId(), NULL);
+
+	delete pFilter;
+	return;
+}
+
+void testLocalVideo()
+{
+	LocalVideo* pVideo = LocalVideo::getInstance();
+
+	pVideo->prepare("/home/mj/disk/videotest/2017/06/06/clip/linshi.mp4");
+	pVideo->initDecoder();
+}
+
+int main() {
+//	testLocalVideo();
+
+	testFilter();
+
+//	printf("hello world");
+
+	return 0;
+}
